a2791e3f5979ccddf763afd722851c565e464c39
Angelika Maria Piątkowska (piatkosia) <piatkosia.apt@interia.pl>
2020-03-14 19:14:03 +0100

    Add project files.

---
+++ SimpleMenuTutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMenuTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
